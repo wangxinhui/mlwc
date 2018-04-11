d17cc77e6617cb34e8a9e9dcd5a54813e396e8b8
wangxinhui <wangxinhuileft@163.com>
2018-04-11 19:44:45 +0800

    mui

--- mlwc-common/src/main/java/com/mlwc/common/util/Test.java
+++ mlwc-common/src/main/java/com/mlwc/common/util/Test.java
@@ -8,20 +8,7 @@ import java.util.Map;
  * @Author: by Administrator on 2018/4/8.
  */
 public class Test {
-    public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException {
-        Map<String, String> keyMap = RSAUtil.createKeys(1024);
-        String  publicKey = keyMap.get("publicKey");
-        String  privateKey = keyMap.get("privateKey");
-        System.out.println("公钥: \n\r" + publicKey);
-        System.out.println("私钥： \n\r" + privateKey);
+    public static void main(String[] args) {
 
-        System.out.println("公钥加密——私钥解密");
-        String str = "123456";
-        System.out.println("\r明文：\r\n" + str);
-        System.out.println("\r明文大小：\r\n" + str.getBytes().length);
-        String encodedData = RSAUtil.publicEncrypt(str, RSAUtil.getPublicKey(publicKey));
-        System.out.println("密文：\r\n" + encodedData);
-        String decodedData = RSAUtil.privateDecrypt(encodedData, RSAUtil.getPrivateKey(privateKey));
-        System.out.println("解密后文字: \r\n" + decodedData);
     }
 }
